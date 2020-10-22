99129d09cf193b132fcadb8b43982c7c924b8b8a
Kyle Rush <kyrush@microsoft.com>
2020-10-21 22:44:39 -0700

    Make factors dynamic

--- plot.js
+++ plot.js
@@ -1,50 +1,52 @@
-const devCostPerWeek = 3000;
-const riskFactorCost = 1000;
-const deprecationWeek = 24;
-const plotWeeks = 52;
-
-function calculateCost(devWeeks, maintenanceCostPerWeek, azureSpendPerWeek, riskFactor, externalTeamCostPerWeek, week) {
-    var devCost = devWeeks * devCostPerWeek * Math.min(devWeeks, week);
-    var maintenanceCost = maintenanceCostPerWeek * week;
-    var azureSpendCost = azureSpendPerWeek * week;
-    var riskCost = riskFactor * riskFactorCost * week;
-    var externalTeamCost = externalTeamCostPerWeek * week;
-    return devCost + maintenanceCost + azureSpendCost + riskCost + externalTeamCost;
-}
-
-function migrateWebjob(week) {
-    return calculateCost(
-        $('#migrate .dev-effort').val(),
-        $('#migrate .maintenance').val(),
-        $('#migrate .azure-spend').val(),
-        $('#migrate .risk-factor').val(),
-        $('#migrate .ext-cost').val(),
-        Math.min(week, deprecationWeek))
-        + migrateLocServices(Math.max(0, week - deprecationWeek));
-}
-
-function maintainWebjob(week) {
-    return calculateCost(
-        $('#maintain .dev-effort').val(),
-        $('#maintain .maintenance').val(),
-        $('#maintain .azure-spend').val(),
-        $('#maintain .risk-factor').val(),
-        $('#maintain .ext-cost').val(),
-        Math.min(week, deprecationWeek))
-        + migrateLocServices(Math.max(0, week - deprecationWeek));
-}
-
-function migrateLocServices(week) {
-    return calculateCost(
-        $('#deprecate .dev-effort').val(),
-        $('#deprecate .maintenance').val(),
-        $('#deprecate .azure-spend').val(),
-        $('#deprecate .risk-factor').val(),
-        $('#deprecate .ext-cost').val(),
-        Math.min(week, deprecationWeek));
-}
-
 function renderPlot() {
+    var deprecationWeek = $("#deprecation-week").val();
+  
+    function calculateCost(devWeeks, maintenanceCostPerWeek, azureSpendPerWeek, riskFactor, externalTeamCostPerWeek, week) {
+        var devCostPerWeek = $("#dev-rate").val();
+        var riskFactorCost = $("#risk-factor-cost").val();
+      
+        var devCost = devWeeks * devCostPerWeek * Math.min(devWeeks, week);
+        var maintenanceCost = maintenanceCostPerWeek * week;
+        var azureSpendCost = azureSpendPerWeek * week;
+        var riskCost = riskFactor * riskFactorCost * week;
+        var externalTeamCost = externalTeamCostPerWeek * week;
+        return devCost + maintenanceCost + azureSpendCost + riskCost + externalTeamCost;
+    }
+
+    function migrateWebjob(week) {
+        return calculateCost(
+            $('#migrate .dev-effort').val(),
+            $('#migrate .maintenance').val(),
+            $('#migrate .azure-spend').val(),
+            $('#migrate .risk-factor').val(),
+            $('#migrate .ext-cost').val(),
+            Math.min(week, deprecationWeek))
+            + migrateLocServices(Math.max(0, week - deprecationWeek));
+    }
+    
+    function maintainWebjob(week) {
+        return calculateCost(
+            $('#maintain .dev-effort').val(),
+            $('#maintain .maintenance').val(),
+            $('#maintain .azure-spend').val(),
+            $('#maintain .risk-factor').val(),
+            $('#maintain .ext-cost').val(),
+            Math.min(week, deprecationWeek))
+            + migrateLocServices(Math.max(0, week - deprecationWeek));
+    }
+    
+    function migrateLocServices(week) {
+        return calculateCost(
+            $('#deprecate .dev-effort').val(),
+            $('#deprecate .maintenance').val(),
+            $('#deprecate .azure-spend').val(),
+            $('#deprecate .risk-factor').val(),
+            $('#deprecate .ext-cost').val(),
+            Math.min(week, deprecationWeek));
+    }
+    
+    var plotWeeks = 52;
+  
     var xAxis = Array(plotWeeks).fill().map((element, index) => index)
 
     var trace1 = {
