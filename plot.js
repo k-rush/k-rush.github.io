47f12a67601709375cee92fb640250641b0e2999
Kyle Rush <kyrush@microsoft.com>
2020-10-21 22:39:36 -0700

    Make chart dynamic

--- plot.js
+++ plot.js
@@ -3,61 +3,77 @@ const riskFactorCost = 1000;
 const deprecationWeek = 24;
 const plotWeeks = 52;
 
-function calculateCost(devWeeks, maintenanceCostPerWeek, azureSpendPerWeek, riskFactor, externalTeamCostPerWeek, week)
-{
-  var devCost = devWeeks * devCostPerWeek * Math.min(devWeeks, week);
-  var maintenanceCost = maintenanceCostPerWeek * week;
-  var azureSpendCost = azureSpendPerWeek * week;
-  var riskCost = riskFactor * riskFactorCost * week;
-  var externalTeamCost = externalTeamCostPerWeek * week;
-  return devCost + maintenanceCost + azureSpendCost + riskCost + externalTeamCost;
+function calculateCost(devWeeks, maintenanceCostPerWeek, azureSpendPerWeek, riskFactor, externalTeamCostPerWeek, week) {
+    var devCost = devWeeks * devCostPerWeek * Math.min(devWeeks, week);
+    var maintenanceCost = maintenanceCostPerWeek * week;
+    var azureSpendCost = azureSpendPerWeek * week;
+    var riskCost = riskFactor * riskFactorCost * week;
+    var externalTeamCost = externalTeamCostPerWeek * week;
+    return devCost + maintenanceCost + azureSpendCost + riskCost + externalTeamCost;
 }
 
-function migrateWebjob(week)
-{
-  var weekNumberBeforeDepracation = Math.min(week, deprecationWeek);
-  var weekNumberAfterDeprecation = Math.max(0, week - deprecationWeek);
-  return calculateCost(3, 500, 100, 5, 100, weekNumberBeforeDepracation) + migrateLocServices(weekNumberAfterDeprecation);
+function migrateWebjob(week) {
+    return calculateCost(
+        $('#migrate .dev-effort').val(),
+        $('#migrate .maintenance').val(),
+        $('#migrate .azure-spend').val(),
+        $('#migrate .risk-factor').val(),
+        $('#migrate .ext-cost').val(),
+        Math.min(week, deprecationWeek))
+        + migrateLocServices(Math.max(0, week - deprecationWeek));
 }
 
-function maintainWebjob(week)
-{
-  var weekNumberBeforeDepracation = Math.min(week, deprecationWeek);
-  var weekNumberAfterDeprecation = Math.max(0, week - deprecationWeek);
-  return calculateCost(0, 1000, 500, 1, 100, weekNumberBeforeDepracation) + migrateLocServices(weekNumberAfterDeprecation);
+function maintainWebjob(week) {
+    return calculateCost(
+        $('#maintain .dev-effort').val(),
+        $('#maintain .maintenance').val(),
+        $('#maintain .azure-spend').val(),
+        $('#maintain .risk-factor').val(),
+        $('#maintain .ext-cost').val(),
+        Math.min(week, deprecationWeek))
+        + migrateLocServices(Math.max(0, week - deprecationWeek));
 }
 
-function migrateLocServices(week)
-{
-  return calculateCost(12, 0, 0, 1, 0, week);
+function migrateLocServices(week) {
+    return calculateCost(
+        $('#deprecate .dev-effort').val(),
+        $('#deprecate .maintenance').val(),
+        $('#deprecate .azure-spend').val(),
+        $('#deprecate .risk-factor').val(),
+        $('#deprecate .ext-cost').val(),
+        Math.min(week, deprecationWeek));
 }
 
-var xAxis = Array(plotWeeks).fill().map((element, index) => index)
+function renderPlot() {
+    var xAxis = Array(plotWeeks).fill().map((element, index) => index)
 
-var trace1 = {
-  x: xAxis,
-  y: Array(plotWeeks).fill().map((element, index) => migrateWebjob(index)),
-  mode: 'lines',
-  name: 'Migrate webjob to function'
-};
+    var trace1 = {
+        x: xAxis,
+        y: Array(plotWeeks).fill().map((element, index) => migrateWebjob(index)),
+        mode: 'lines',
+        name: 'Migrate webjob to function'
+    };
 
-var trace2 = {
-  x: xAxis,
-  y: Array(plotWeeks).fill().map((element, index) => maintainWebjob(index)),
-  mode: 'lines',
-  name: 'Maintain webjob'
+    var trace2 = {
+        x: xAxis,
+        y: Array(plotWeeks).fill().map((element, index) => maintainWebjob(index)),
+        mode: 'lines',
+        name: 'Maintain webjob'
+    }
+
+    var trace3 = {
+        x: xAxis,
+        y: Array(plotWeeks).fill().map((element, index) => migrateLocServices(index)),
+        mode: 'lines',
+        name: 'Migrate to standard localization'
+    }
+
+    var data = [trace1, trace2, trace3];
+
+    Plotly.newPlot('plot', data);
 }
-
-var trace3 = {
-  x: xAxis,
-  y: Array(plotWeeks).fill().map((element, index) => migrateLocServices(index)),
-  mode: 'lines',
-  name: 'Migrate to standard localization'
-}
-
-
-var data = [ trace1, trace2, trace3 ];
 
 window.addEventListener('load', (event) => {
-    Plotly.newPlot('plot', data);
+    renderPlot();
+    $(":input").change(renderPlot);
 });
